59bc40ca12a7c8079b1a152f55aafc2403a87f73
Tonnyderrick <tonnyaketch@gmail.com>
2025-05-21 18:05:17 +0300

    decorators

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,22 @@
+import sqlite3
+import functools
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if args:
+            print(f"Executing SQL Query: {args[0]}")
+        elif 'query' in kwargs:
+            print(f"Executing SQL Query: {kwargs['query']}")
+        return func(*args, **kwargs)
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+users = fetch_all_users(query="SELECT * FROM users")
