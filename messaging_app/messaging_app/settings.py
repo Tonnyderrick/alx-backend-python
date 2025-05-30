a33d4181e242542812fff1a4b96eab41eac4e680
Tonnyderrick <tonnyaketch@gmail.com>
2025-05-30 18:03:52 +0300

    mess app

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -1,5 +1,17 @@
+# messaging_app/settings.py
+
 INSTALLED_APPS = [
     ...
     'rest_framework',
     'chats',
 ]
+
+# Django REST Framework config
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
