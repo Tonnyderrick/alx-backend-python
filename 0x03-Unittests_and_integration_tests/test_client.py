2d3f72aacb7929b8de32b7c3d6fd87e3342668a6
Tonnyderrick <tonnyaketch@gmail.com>
2025-05-30 16:52:24 +0300

    m

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
+"""
+Unit test for GithubOrgClient.org
+"""
+
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
 
 from client import GithubOrgClient
 
+
 class TestGithubOrgClient(unittest.TestCase):
-    """Tests for GithubOrgClient"""
+    """Test case for GithubOrgClient"""
 
     @parameterized.expand([
         ("google",),
         ("abc",)
     ])
-    @patch("client.get_json")
+    @patch("client.get_json") 
     def test_org(self, org_name, mock_get_json):
-        """Test that GithubOrgClient.org returns correct payload"""
-        mock_response = {"login": org_name}
-        mock_get_json.return_value = mock_response
+        """Test that GithubOrgClient.org returns the correct value"""
+        expected = {"login": org_name}
+        mock_get_json.return_value = expected
 
         client = GithubOrgClient(org_name)
-        self.assertEqual(client.org, mock_response)
+        self.assertEqual(client.org, expected)
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+
 
 if __name__ == "__main__":
     unittest.main()
