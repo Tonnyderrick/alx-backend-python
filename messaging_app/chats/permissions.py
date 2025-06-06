32079930417ba4815b7e434933d5074621df8122
Tonnyderrick <tonnyaketch@gmail.com>
2025-06-06 20:40:37 +0300

    0. Implement Authentication

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,5 +1,13 @@
+from rest_framework import permissions
 from rest_framework.permissions import BasePermission
 
+
 class IsParticipantOfConversation(BasePermission):
+    """
+    Custom permission to allow only participants of a conversation
+    to access messages or conversation objects.
+    """
+
     def has_object_permission(self, request, view, obj):
+        # Ensure the requesting user is a participant of the conversation
         return request.user in obj.participants.all()
