881821658bf5dfcdd2c41d294822f4eba369b61a
Tonnyderrick <tonnyaketch@gmail.com>
2025-07-30 21:20:02 +0300

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+Unittest module for testing memoization in utils.memoize decorator
+"""
+
 import unittest
 from unittest.mock import patch
 from utils import memoize
@@ -18,13 +22,10 @@ class TestMemoize(unittest.TestCase):
         with patch.object(TestClass, 'a_method', return_value=42) as mocked_method:
             obj = TestClass()
 
-            # Call the memoized method twice
             result1 = obj.a_property()
             result2 = obj.a_property()
 
-            # Assert result is correct
             self.assertEqual(result1, 42)
             self.assertEqual(result2, 42)
 
-            # Assert a_method is only called once due to memoization
             mocked_method.assert_called_once()
