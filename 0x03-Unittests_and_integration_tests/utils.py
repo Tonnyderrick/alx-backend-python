99b9094117b06afa7223be8a3da21832ca59a413
Tonnyderrick <tonnyaketch@gmail.com>
2025-07-30 22:44:53 +0300

    m

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,26 +1,13 @@
-# utils.py
-
-import requests
-from functools import wraps
-
-def access_nested_map(nested_map, path):
-    """Access a nested map using a sequence of keys."""
-    for key in path:
-        nested_map = nested_map[key]
-    return nested_map
-
-def get_json(url):
-    """Get JSON content from a URL."""
-    response = requests.get(url)
-    return response.json()
+#!/usr/bin/env python3
+"""Utils module with memoize"""
 
 def memoize(method):
-    """Memoization decorator to cache method results."""
-    attr_name = f"_memoized_{method.__name__}"
+    """Decorator to cache method output in instance attributes"""
+    attr_name = "_{}".format(method.__name__)
 
-    @wraps(method)
     def wrapper(self):
         if not hasattr(self, attr_name):
             setattr(self, attr_name, method(self))
         return getattr(self, attr_name)
+
     return wrapper
