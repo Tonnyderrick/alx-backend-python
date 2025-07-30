05f3b7cfdc4779b7b220b12871897fabfc7cddb1
Tonnyderrick <tonnyaketch@gmail.com>
2025-07-30 20:59:31 +0300

    Parameterize a unit test

---
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -0,0 +1,7 @@
+# utils.py
+
+def access_nested_map(nested_map, path):
+    """Access a nested map using a sequence of keys."""
+    for key in path:
+        nested_map = nested_map[key]
+    return nested_map
