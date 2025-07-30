afd6e44bfb31e7b2a7887c114d04f51d6c5d1e19
Tonnyderrick <tonnyaketch@gmail.com>
2025-07-30 22:20:38 +0300

    Parameterize

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -1,34 +1,42 @@
 #!/usr/bin/env python3
-""" GitHub Org Client """
-
-from typing import Dict, List
+"""Github Org Client"""
+from typing import List
+from functools import lru_cache
 import requests
 
 
-def get_json(url: str) -> Dict:
-    """Return JSON content from the given URL"""
-    return requests.get(url).json()
+def get_json(url: str) -> dict:
+    """Return JSON content from a REST API endpoint."""
+    response = requests.get(url)
+    return response.json()
 
 
 class GithubOrgClient:
-    """GitHub Org Client"""
+    """Github Organization Client"""
 
-    ORG_URL = "https://api.github.com/orgs/{}"
+    ORG_URL = "https://api.github.com/orgs/{org}"
 
     def __init__(self, org_name: str) -> None:
+        """Initialize with organization name"""
         self._org_name = org_name
 
     @property
-    def org(self) -> Dict:
-        """Fetch organization data"""
-        return get_json(self.ORG_URL.format(self._org_name))
+    @lru_cache()
+    def org(self) -> dict:
+        """Cached organization data"""
+        return get_json(self.ORG_URL.format(org=self._org_name))
 
     @property
     def _public_repos_url(self) -> str:
-        """Extract and return public repositories URL"""
+        """Return the public repos URL"""
         return self.org.get("repos_url")
 
     def public_repos(self) -> List[str]:
         """Get list of public repository names"""
         repos = get_json(self._public_repos_url)
         return [repo["name"] for repo in repos]
+
+    @staticmethod
+    def has_license(repo: dict, license_key: str) -> bool:
+        """Check if repo has the given license"""
+        return repo.get("license", {}).get("key") == license_key
